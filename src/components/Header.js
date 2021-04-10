b1d780e9209964fc10226fcc09270ff5b142c250
Nick Tam <nbilly315@gmail.com>
2021-04-10 12:21:57 -0700

    Fix login bug

--- src/components/Header.js
+++ src/components/Header.js
@@ -37,7 +37,7 @@ function Header() {
             </div>
 
             <div className="header__nav">
-                <Link to={!user ? "/" : "/login"}>
+                <Link to={user ? "/" : "/login"}>
                 <div className="header__option" onClick={handleAuthentication}>
                     <span className="header__optionLineOne">Hello {user ? user?.email : "Guest"}</span>
                     <span className="header__optionLineTwo">Sign {user ? "Out" : "In"}</span>
