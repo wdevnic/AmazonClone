7cba65de8d6279f632c4e1a1d203893b94cb7d31
Nick Tam <nbilly315@gmail.com>
2021-04-10 00:42:54 -0700

    use env variables

---
+++ src/firebase.js
@@ -0,0 +1,24 @@
+import firebase from 'firebase'
+
+const API_KEY = process.env.REACT_APP_API_KEY;
+const AUTH_DOMAIN = process.env.REACT_APP_AUTH_DOMAIN ;
+const PROJECT_ID = process.env.REACT_APP_PROJECT_ID ;
+const STORAGE_BUCKET= process.env.REACT_APP_STORAGE_BUCKET ;
+const MESSAGING_SENDER_ID = process.env.REACT_APP_MESSAGING_SENDER_ID ;
+const APP_ID= process.env.REACT_APP_APP_ID ;
+
+const firebaseConfig = {
+    apiKey: API_KEY,
+    authDomain: AUTH_DOMAIN,
+    projectId: PROJECT_ID,
+    storageBucket: STORAGE_BUCKET,
+    messagingSenderId: MESSAGING_SENDER_ID,
+    appId: APP_ID
+  };
+
+  const firebaseApp = firebase.initializeApp(firebaseConfig);
+
+  const db = firebaseApp.firestore();
+  const auth = firebaseApp.auth();
+
+  export {db, auth};
